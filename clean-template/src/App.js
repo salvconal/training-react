64ba7513e794d6db9d8f0b768a604c786c83801f
salvador <salvconal@gmail.com>
2024-03-25 12:50:56 +0100

    cleanTemplateReady

---
+++ clean-template/src/App.js
@@ -0,0 +1,13 @@
+
+//import './App.css';
+
+function App() {
+  return (
+    <div> 
+      <h1>Hello World</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste magnam qui voluptates laudantium labore? Porro aspernatur quo officia iusto totam quia ipsa fugiat sequi quasi, vel neque nobis? Harum distinctio, ea eos expedita tenetur quis pariatur sed, excepturi assumenda incidunt id. Modi amet odit iure perferendis quidem nam cupiditate odio!</p>
+    </div>
+  );
+}
+
+export default App;
